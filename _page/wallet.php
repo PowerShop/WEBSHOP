0efa86550019c0891df119055c968cf3b7364193
PowerShopMCPE <37243611+PowerShop@users.noreply.github.com>
2019-01-17 16:24:42 +0700

    Add files via upload

--- _page/wallet.php
+++ _page/wallet.php
@@ -8,17 +8,71 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   </head>
-  <body>
-      <div class="card">
+  <body class="col-md-12 font mx-auto mt-2">
+  <?php include 'menu.php';?>
+      <div class="card bg-warning mt-2">
           <img class="card-img-top"  alt="">
-          <div class="card-body bg-warning">
-              <h4 class="card-title">Title</h4>
-              <p class="card-text">Text</p>
+          <div class="card-body text-center">
+              <h4 class="card-title"><i class="fa fa-money" aria-hidden="true"></i> Refill</h4>
+              <p class="card-text">Refill by : <a href="?page=wallet&topup=truemoney">TrueMoney</a> | <a href="?page=wallet&topup=truewallet">TrueWallet</a></p>
           </div>
       </div>
+      <?php if (isset($_GET['topup'])) {
+    if ($_GET['topup'] == 'truemoney') {
+        ?>
+     <div class="card mt-2">
+          <img class="card-img-top" alt="">
+          <div class="card-body">
+          <form action="" method="post">
+              <h4 class="card-title text-center">TrueMoney</h4>
+              <div class="form-group">
+                <label for="cash"><i class="fa fa-sort-numeric-asc" aria-hidden="true"></i> CashCard Number</label>
+                <input type="text" name="cash" id="cash" class="form-control" placeholder="1234567890000" aria-describedby="helpId">
+                <small id="helpId" class="text-muted">หมายเลขบัตรทรูมันนี่ 14 หลัก</small>
+              </div>
+              <div class="form-group" style="text-align: center;">
+              <button type="submit" class="btn btn-success" name="refill"><i class="fa fa-check-circle" aria-hidden="true"></i> ดำเนินการเติมเงิน</button>
+              </div>
+              </form>
+          </div>
+      </div>
+      <?php
+}
+}?>
+<?php if (isset($_GET['topup'])) {
+    if ($_GET['topup'] == 'truewallet') {
+        ?>
+    <div class="card mt-2">
+          <img class="card-img-top" alt="">
+          <div class="card-body">
+              <h4 class="card-title text-center">TrueWallet</h4>
+              <form action="" method="post">
+              <div class="form-group">
+                <label for="tranid"><i class="fa fa-sort-numeric-asc" aria-hidden="true"></i> หมายเลขอ้างอิง</label>
+                <input type="text" name="tranid" id="tranid" class="form-control" placeholder="Transaction Id" aria-describedby="helpId">
+                <small id="helpId" class="text-muted">หมายเลขอ้างอิง 14 หลัก</small>
+              </div>
+              <div class="form-group" style="text-align: center;">
+              <button type="submit" name="refill" class="btn btn-success"><i class="fa fa-check-circle" aria-hidden="true"></i> ดำเนินการเติมเงิน</button>
+              </div>
+              </form>
+          </div>
+      </div>
+      <?php
+}
+}?>
+<?php if (isset($_GET['topup']) == null) {?>
+<div class="card mt-2">
+    <img class="card-img-top" alt="">
+    <div class="card-body">
+        <h4 class="card-title text-center">กรุณาเลือกวิธีการเติมเงิน</h4>
+    </div>
+</div>
+<?php }?>
+<?php include 'footer.php';?>
+<div class="mt-2"></div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -27,49 +81,79 @@
   </body>
 </html>
 <?php
+@ini_set('display_errors', '0');
+date_default_timezone_set("Asia/Bangkok");
+$time = trim(date("d-m-Y H:i:s"));
+if (isset($_POST['refill'])) {
+    // Login
+    $token = $api->wallet->GetToken('aaaxcvg@gmail.com', 'Jonathan1997');
 
-// Login
-$token = $api->wallet->GetToken('aaaxcvg@gmail.com', 'Jonathan1997');
+    // If successfully login
 
-// If successfully login
-if ($token != null) {
-    // Transaction date range
-    $start_date = date('Y-m-d', strtotime('-100 days'));
-    $end_date = date('Y-m-d', strtotime('1 days'));
+    if ($token != null) {
+        if ($_POST['tranid']) {
+            // Transaction date range
+            $start_date = date('Y-m-d', strtotime('-100 days'));
+            $end_date = date('Y-m-d', strtotime('1 days'));
 
-    // Perform Fetch
-    $activities = $api->wallet->FetchActivities($token, $start_date, $end_date);
+            // Perform Fetch
+            $activities = $api->wallet->FetchActivities($token, $start_date, $end_date);
 
-    foreach ($activities as $arr) {
-        // Check is paid-in
-        if ($arr['text3En'] == 'creditor') {
-            $data = $api->wallet->FetchTxDetail($token, $arr['reportID']);
+            foreach ($activities as $arr) {
+                // Check is paid-in
+                if ($arr['text3En'] == 'creditor') {
+                    $data = $api->wallet->FetchTxDetail($token, $arr['reportID']);
 
-            // Transaction ID
-            $tx['id'] = $data['section4']['column2']['cell1']['value'];
+                    // Transaction ID
+                    $tx['id'] = $data['section4']['column2']['cell1']['value'];
 
-            // Amount
-            $tx['amount'] = str_replace(',', '', $data['section3']['column1']['cell1']['value']);
+                    // Amount
+                    $tx['amount'] = str_replace(',', '', $data['section3']['column1']['cell1']['value']);
 
-            // Then you can check user input and connect to database.
-            $ref = $_GET['id'];
-            if ($tx['id'] === $ref) {
-                echo 'หมายเลขอ้างอิง '.$tx['id'].' <br/> จำนวนเงิน '.$tx['amount'].' บาท';
+                    // Then you can check user input and connect to database.
+                    $ref = $_POST['tranid'];
+                    if ($tx['id'] == $ref) {
+                        if (query("SELECT * FROM history WHERE truewallet = '$ref'")->rowCount() == 1) {
+                            echo '<script type="text/javascript">
+                        swal("Error","เติมเงินไม่สำเร็จ \n หมายเลขอ้างอิง ' . $ref . ' ถูกใช้งานเเล้ว","error");
+                        </script>';
+                        break;
+                        } else {
+                            $username = $_SESSION['username'];
+                            $amount = $tx['amount'];
+                            query("INSERT INTO `history`(`truewallet`,`amount`,`name`,`date`) VALUES('" . $ref . "','" . $amount . "','" . $username . "','" . $time . "')");
+                            echo '<script type="text/javascript">
+                        swal("Success","เติมเงินสำเร็จ ' . $tx['amount'] . ' บาท","success");
+                        </script>';
+                        break;
+                        }
+                    }elseif ($tx['id'] !== $ref) {
+                        echo '<script type="text/javascript">
+                        swal("Error","เติมไม่เงินสำเร็จ หมายเลขอ้างอิงไม่มีอยู่ในระบบ","error");
+                        </script>';
+                        break;
+                    }
+                }
             }
         }
-    }
-    if (isset($_GET['cashtopup'])) {
-        $cashcard = $_GET['cashtopup'];
-        $topup = json_decode($api->wallet->CashcardTopup($token, $cashcard));
-
-        if (isset($topup->amount)) {
-            echo 'Topup Success '.$topup->amount.'';
-        } elseif (isset($topup->code)) {
-            if ($topup->code < 0) {
-                echo 'Topup Failed';
+        if ($_POST['cash']) {
+            $cashcard = $_POST['cash'];
+            $topup = json_decode($api->wallet->CashcardTopup($token, $cashcard));
+            if (isset($topup->amount)) {
+                if (query('SELECT * FROM `user` WHERE `username` =?;', array($username))->rowCount() == 1) {
+                    echo '<script type="text/javascript">
+                        swal("Success","เติมเงินสำเร็จ ' . $topup->amount . ' บาท","success");
+                        </script>';
+                }
+            } elseif (isset($topup->code)) {
+                if ($topup->code < 0) {
+                    echo '<script type="text/javascript">
+                    swal("Error","เติมไม่เงินสำเร็จ หมายเลขบัตรทรูมันนี่ไม่ถูกต้อง","error");
+                    </script>';
+                }
             }
         }
+        // Logout
+        $api->wallet->Logout($token);
     }
-    // Logout
-    $api->wallet->Logout($token);
 }
