3fe4aa852aaada50559046517d7129c744019a3c
PowerShopMCPE <37243611+PowerShop@users.noreply.github.com>
2019-02-11 18:24:44 +0700

    อัพเดท Url Api

--- _page/status.php
+++ _page/status.php
@@ -2,7 +2,7 @@
 @ini_set('display_errors', '0');
 require '../_sys/_config.php';
 $query = (object) array(
-    "api" => json_decode(file_get_contents("https://mcapi-th.cf/mcapi/v1/status/api.php?host=" . $_config['mc_host'] . "&port=" . $_config['mc_port'] . "")),
+    "api" => json_decode(file_get_contents("http://localhost/_sys/_status/_api.php?host=" . $_config['mc_host'] . "&port=" . $_config['mc_port'] . "")),
 );
 echo $query->api->players->online;
 echo " / ";
