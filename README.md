342babe80a6b72d44eafd0e018fa5d48159a25a2
PowerShopMCPE <37243611+PowerShop@users.noreply.github.com>
2021-08-19 02:05:26 +0700

    update code & fix bug

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # WEBSHOP
 This Is A WEBSHOP <br /> 
-มันออกจะเเมวๆไปหน่อยนะครับสำหรับเว็บนี้ 55555 
+สามารถโหลดไปใช้งานกันได้ฟรี ๆ เลยนะครับโค้ดอาจจะไม่สวยงามมากนักเพราะเป็นโปรเจคที่ทำไว้นานแล้วครับตั้งแต่ปี 2019
 <br />
 # Latest update <br />
-Wednesday 10 April 2019 22:38 PM
+Wednesday 19 August 2021 02:03 AM
 
 # Version <br />
-Lastest Version 1.0
+Lastest Version 1.1
 
 # Contact <br />
 <a href="https://fb.me/Min.Sakmueang"><u>Facebook</u></a>
--- _sys/_config.php
+++ _sys/_config.php
@@ -5,7 +5,7 @@ $_config = array();
 $_config['db_host'] = 'localhost'; // Host ของท่าน Default "localhost"
 $_config['db_database'] = 'webshop'; // ชื่อฐานข้อมูล Default "webshop"
 $_config['db_user'] = 'root'; // ชื่อผู้ใช้ Phpmyadmin Default "root"
-$_config['db_password'] = '###'; // รหัสผ่าน Phpmyadmin Default "ไม่มี"
+$_config['db_password'] = ''; // รหัสผ่าน Phpmyadmin Default "ไม่มี"
 /* Config ด้านล่างนี้สำหรับเเสดง Status Server ที่หน้า Home */
 $_config['mc_host'] = '203.150.243.93';
 $_config['mc_port'] = '19132';
